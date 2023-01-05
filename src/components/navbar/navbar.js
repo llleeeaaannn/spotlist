9da8ecd115f349925366bbee5322122e13c41df6
llleeeaaannn <piercefrank10@gmail.com>
2023-01-05 19:22:33 +0000

    Added wave to header and other styling changes, navbar changed colour

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,14 +6,21 @@ const Navbar = () => {
   const [selected, setSelected] = useState(false);
 
   return (
-    <nav className={styles.navbar}>
-      <div>
-        <h1>Spotlist</h1>
+    <nav>
+      <div className={styles.navbar}>
+        <div>
+          <h1>Spotlist</h1>
+        </div>
+        <a href="https://github.com/llleeeaaannn/spotlist" className={styles.github} target='_blank' rel='noopener noreferrer'>
+          <svg viewBox="0 0 24 24">
+            <path d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/>
+          </svg>
+        </a>
       </div>
-      <div className={selected ? styles.selectedHamburger : styles.hamburger} onClick={() => setSelected(!selected)}>
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
+      <div className={styles.wave}>
+        <svg viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
+        </svg>
       </div>
     </nav>
   )
