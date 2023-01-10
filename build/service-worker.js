5530a7025d00e5ec63819fbca580c619efb0f370
llleeeaaannn <piercefrank10@gmail.com>
2023-01-10 16:52:30 +0000

    Testing service worker caching"
    "

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,8 +1,19 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Install");
+const CACHE_NAME = "version-1";
+
+const urlsToCache = [
+  'index.html',
+  'static/js/main.js'
+];
+
+// Install SW
+self.addEventListener('install', (event) => {
+  console.log('HECK TESTING');
+  console.log('Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  )
 });
-
-
-self.addEventListener('fetch', (event) => {
-  console.log('FETCH');
-})
--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -1,8 +1,18 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Install");
+const CACHE_NAME = "version-1";
+
+const urls = [
+  'index.html',
+  'static/js/main.js',
+  'static/css/styles.css'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urls);
+      })
+  )
 });
-
-
-self.addEventListener('fetch', (event) => {
-  console.log('FETCH');
-})
