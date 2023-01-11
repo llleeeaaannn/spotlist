5f72122240f782a71ad0411f8f8cb5d5f8e074c7
llleeeaaannn <piercefrank10@gmail.com>
2023-01-11 04:55:17 +0000

    Removed most of SW

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -4,6 +4,7 @@ const urls = [
   'index.html',
   'static/js/main.dc762096.js',
   'static/css/main.09b63fcd.css'
+  '/static/media/GTUltra-Regular.41205ca9d5907eb1575a.woff'
 ];
 
 self.addEventListener('install', (event) => {
