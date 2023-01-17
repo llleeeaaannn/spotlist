6962a743a79cbe3ffc2e7ce64b5182da2809ab7d
llleeeaaannn <piercefrank10@gmail.com>
2023-01-17 02:57:44 +0000

    Added to SW

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -1,1 +1,18 @@
-const CACHE_NAME = "version-2";
+const CACHE_NAME = "cache-v1";
+
+const CACHE_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+]
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(CACHE_ASSETS);
+      }
+    )
+    // .then(() => self.skipWaiting())
+  );
+});
